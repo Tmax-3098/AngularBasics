72bd639fa8e7c6e1b7a97633dfa0a2f8e2b61bd1
Tmax-3098 <ishaan.3098@gmail.com>
2026-01-27 10:52:30 +0530

    adding user component and property binding

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+import { DUMMY_USERS } from '../dummy-user';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+
+  selectedUser= DUMMY_USERS[randomIndex];
+  imagePath:String = "assets/users/"+this.selectedUser.avatar;
+
+}
